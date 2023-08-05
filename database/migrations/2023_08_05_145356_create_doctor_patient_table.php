e0d4eefdf8207b31b2687bed7173f1ba236cb340
VandoJunqueira <vando_junqueira@hotmail.com>
2023-08-05 11:38:31 -0400

    first commit

---
+++ database/migrations/2023_08_05_145356_create_doctor_patient_table.php
@@ -0,0 +1,32 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     */
+    public function up(): void
+    {
+        Schema::create('doctor_patient', function (Blueprint $table) {
+            $table->id();
+            $table->bigInteger('doctor_id')->unsigned();
+            $table->bigInteger('patient_id')->unsigned();
+            $table->timestamps();
+
+            $table->foreign('doctor_id')->references('id')->on('doctors');
+            $table->foreign('patient_id')->references('id')->on('patients');
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     */
+    public function down(): void
+    {
+        Schema::dropIfExists('doctor_patient');
+    }
+};
