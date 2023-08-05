ad046d03ac78145fadb6f51586007b068d196828
VandoJunqueira <vando_junqueira@hotmail.com>
2023-08-05 11:54:49 -0400

    feat: Adicionar models

--- database/factories/DoctorFactory.php
+++ database/factories/DoctorFactory.php
@@ -3,7 +3,6 @@
 namespace Database\Factories;
 
 use Illuminate\Database\Eloquent\Factories\Factory;
-use Illuminate\Support\Str;
 
 /**
  * @extends \Illuminate\Database\Eloquent\Factories\Factory<\App\Models\User>
@@ -17,22 +16,32 @@ class DoctorFactory extends Factory
      */
     public function definition(): array
     {
-        return [
-            'name' => fake()->name(),
-            'email' => fake()->unique()->safeEmail(),
-            'email_verified_at' => now(),
-            'password' => '$2y$10$92IXUNpkjO0rOQ5byMi.Ye4oKoEa3Ro9llC/.og/at2.uheWG/igi', // password
-            'remember_token' => Str::random(10),
+        $medicalSpecialties  = [
+            'Cardiologista',
+            'Dermatologista',
+            'Pediatra',
+            'Oncologista',
+            'Ginecologista',
+            'Urologista',
+            'Neurologista',
+            'Ortopedista',
+            'Otorrinolaringologista',
+            'Psiquiatra',
+            'Endocrinologista',
+            'Infectologista',
+            'Pneumologista',
+            'Oftalmologista',
+            'Radiologista',
+            'Gastroenterologista',
+            'Nefrologista',
+            'Cirurgião Plástico',
+            'Hematologista',
+            'Reumatologista',
         ];
-    }
 
-    /**
-     * Indicate that the model's email address should be unverified.
-     */
-    public function unverified(): static
-    {
-        return $this->state(fn (array $attributes) => [
-            'email_verified_at' => null,
-        ]);
+        return [
+            'name' => $this->faker->name,
+            'specialty' => $this->faker->randomElement($medicalSpecialties),
+        ];
     }
 }
